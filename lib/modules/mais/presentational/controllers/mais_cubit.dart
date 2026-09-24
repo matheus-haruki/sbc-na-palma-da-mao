31abc93e1ddb292edc9ec79f4fb7afa1f749c41c
RickDevSantos <ricksantos.dev@gmail.com>
2026-09-24 10:47:39 -0300

    refactor: atualiza MaisCubit para gerenciar o ThemeMode utilizando o AppThemeController

--- lib/modules/mais/presentational/controllers/mais_cubit.dart
+++ lib/modules/mais/presentational/controllers/mais_cubit.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:palma_da_mao/core/theme/app_theme_controller.dart';
 import 'mais_state.dart';
 
 class MaisCubit extends Cubit<MaisState> {
@@ -6,12 +8,11 @@ class MaisCubit extends Cubit<MaisState> {
 
   void carregarConfiguracoes() {
     emit(MaisLoading());
-    emit(MaisSuccess(isDarkMode: false));
+    emit(MaisSuccess(themeMode: AppThemeController.instance.themeMode));
   }
 
-  void toggleTheme(bool value) {
-    if (state is MaisSuccess) {
-      emit(MaisSuccess(isDarkMode: value));
-    }
+  void alterarTema(ThemeMode themeMode) {
+    AppThemeController.instance.setThemeMode(themeMode);
+    emit(MaisSuccess(themeMode: themeMode));
   }
 }
