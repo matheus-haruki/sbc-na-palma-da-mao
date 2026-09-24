e75da942969b552b91e47002baf17b06589f29fc
RickDevSantos <ricksantos.dev@gmail.com>
2026-09-24 09:31:03 -0300

    feat: inicializa o AppThemeController de forma assíncrona no main

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
+import 'core/theme/app_theme_controller.dart';
 
-void main() {
+Future<void> main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await AppThemeController.instance.init();
 
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
